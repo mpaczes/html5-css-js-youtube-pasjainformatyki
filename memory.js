24e686242c227fb8efa1165a5721aa025d247c54
Marek Paczesny <marek.paczesny@nokia.com>
2018-03-28 14:49:18 +0200

    obsluga klikniecia kart z jQuery

--- memory.js
+++ memory.js
@@ -3,65 +3,51 @@ var cards = ["ciri.png", "geralt.png", "jaskier.png", "jaskier.png", "iorweth.pn
 
 // console.log(cards);
 
-/* addEventListener() nie zadziala w IE < 9, bo tam jest attachEvent() */
-var c0 = document.getElementById('c0');
-var c1 = document.getElementById('c1');
-var c2 = document.getElementById('c2');
-var c3 = document.getElementById('c3');
-var c4 = document.getElementById('c4');
-var c5 = document.getElementById('c5');
-var c6 = document.getElementById('c6');
-var c7 = document.getElementById('c7');
-var c8 = document.getElementById('c8');
-var c9 = document.getElementById('c9');
-var c10 = document.getElementById('c10');
-var c11 = document.getElementById('c11');
-
-c0.addEventListener("click", function() {
+$("#c0").on("click", function() {
 	revealCard(0);
 });
 
-c1.addEventListener("click", function() {
+$("#c1").on("click", function() {
 	revealCard(1);
 });
 
-c2.addEventListener("click", function() {
+$("#c2").on("click", function() {
 	revealCard(2);
 });
 
-c3.addEventListener("click", function() {
+$("#c3").on("click", function() {
 	revealCard(3);
 });
 
-c4.addEventListener("click", function() {
+$("#c4").on("click", function() {
 	revealCard(4);
 });
 
-c5.addEventListener("click", function() {
+$("#c5").on("click", function() {
 	revealCard(5);
 });
 
-c6.addEventListener("click", function() {
+$("#c6").on("click", function() {
 	revealCard(6);
 });
 
-c7.addEventListener("click", function() {
+$("#c7").on("click", function() {
 	revealCard(7);
 });
 
-c8.addEventListener("click", function() {
+$("#c8").on("click", function() {
 	revealCard(8);
 });
 
-c9.addEventListener("click", function() {
+$("#c9").on("click", function() {
 	revealCard(9);
 });
 
-c10.addEventListener("click", function() {
+$("#c10").on("click", function() {
 	revealCard(10);
 });
 
-c11.addEventListener("click", function() {
+$("#c11").on("click", function() {
 	revealCard(11);
 });
 
