1edceaace9e69373a0ae8ca8a50011beb1e45f04
Marek Paczesny <marek.paczesny@nokia.com>
2018-03-28 13:14:45 +0200

    plansza go gry

---
+++ memory.js
@@ -0,0 +1,26 @@
+
+var cards = ["ciri.png", "geralt.png", "jaskier.png", "jaskier.png", "iorweth.png", "triss.png", "geralt.png", "yen.png", "ciri.png", "triss.png", "yen.png", "iorweth.png"];
+
+console.log(cards);
+
+// funkcja odslon karte
+/*
+	Ktora karta?
+	
+		Pierwsza :
+			Czekamy na druga karte !
+			
+		Druga :
+			Mamy pare?
+				tak :
+					Usun obie karty
+					Licznik tur ++
+					Wygralismy ?
+					
+				nie :
+					Zakryj obie karty
+					Licznik tur ++
+*/
+function revealCard(nr) {
+	alert(nr);
+}
